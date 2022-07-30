5f6b10936c1516b38a41102b26ceb43fa0d4e99e
Jonathan Santos <jonathanjsantos@icloud.com>
2022-07-29 21:32:04 -0300

    updated
    Falta implementar limpar seleções

--- changecolor/script.js
+++ changecolor/script.js
@@ -1,30 +1,25 @@
 var colors = []
 var status_btn_closer = false
 
-function isButtonActive(objeto) {
-    if (colors.indexOf(objeto.id) != -1) {
+function isButtonActive(page_button) {
+    if (colors.indexOf(page_button.id) != -1) {
         return true
     } else {
         return false
     }
 }
 
-function cleanSelections(){
-    if(colors.lenght >=1){
-        while(colors.lenght){
-            colors.splice(0,1)
-            for(let pos in colors){
-                colors[pos]
-            }
-        }
-
-        closerButton()
+function cleanSelections() {
+    for (let pos in colors) {
+        let button = document.getElementById(`${colors[pos]}`)
+        offButton(button)
+        colors.splice(colors[pos], 1)
     }
+    closerButton()
 }
 
 function closerButton() {
-
-    if (colors.length >=1 && status_btn_closer == false) {
+    if (colors.length >= 1 && status_btn_closer == false) {
         let button = document.createElement('div')
         let header = document.getElementById('header')
         button.setAttribute("id", "closer")
@@ -35,38 +30,34 @@ function closerButton() {
         button.setAttribute("background-size", "contain")
         header.appendChild(button)
         status_btn_closer = true
-    } else if (status_btn_closer==true && colors.length==0) {
+    } else if (status_btn_closer == true && colors.length == 0) {
         let button = document.getElementById('closer')
         button.remove()
         status_btn_closer = false
-/*        
-        for (let pos in colors) {
-            let botoes = document.getElementById(`${colors[pos].text}`)
-            botoes.style.removeProperty("filter")
-            bototes.style.boxShadow = "0px 0px 5px rgba(0, 0, 0, 0.384)"
-
-            colors.splice(0, colors[pos])
-        }
-        */
     }
-
 }
 
-function changed(objeto) {
-    let button = document.getElementById(objeto.id)
+function offButton(button) {
+    colors.splice(colors.indexOf(button.id), 1) //retira page_button do array de cores selecionadas
+    button.style.removeProperty("filter")
+    button.style.boxShadow = "0px 0px 5px rgba(0, 0, 0, 0.384)"
+    closerButton()
+}
 
-    if (isButtonActive(objeto)) {
-        colors.splice(colors.indexOf(objeto.id), 1) //retira objeto do array de cores selecionadas
-        button.style.removeProperty("filter")
-        button.style.boxShadow = "0px 0px 5px rgba(0, 0, 0, 0.384)"
-        closerButton()
+function onButton(button) {
+    button.style.boxShadow = "0px 0px 5px white"
+    button.style.filter = "brightness(120%)"
+    colors.push(button.id) //coloca page_button no array de cores selecionadas
+    closerButton()
+}
 
+function changedButton(page_button) {
+    let button = document.getElementById(page_button.id)
+
+    if (isButtonActive(page_button)) {
+        offButton(button);
     } else {
-        button.style.boxShadow = "0px 0px 5px white"
-        button.style.filter = "brightness(120%)"
-        colors.push(objeto.id) //coloca objeto no array de cores selecionadas
-        closerButton()
-
-        //window.alert(`--var(${objeto.id})`)
+        onButton(button);
+        //window.alert(`--var(${page_button.id})`)
     }
 }
