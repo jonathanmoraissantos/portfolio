f3d1c9ec750b460f5d7961050a8440966c451040
Jonathan Santos <jonathanjsantos@icloud.com>
2022-07-30 01:58:47 -0300

    update
    ainda preso no cleanselections()

--- changecolor/script.js
+++ changecolor/script.js
@@ -9,13 +9,43 @@ function isButtonActive(page_button) {
     }
 }
 
+function changedButton(page_button) {
+    let button = document.getElementById(page_button.id)
+
+    if (isButtonActive(page_button)) {
+        offButton(button);
+    } else {
+        onButton(button);
+        //window.alert(`--var(${page_button.id})`)
+    }
+}
+
+function offButton(button) {
+    button.style.removeProperty("filter")
+    button.style.boxShadow = "0px 0px 5px rgba(0, 0, 0, 0.384)"
+    colors.splice(colors.indexOf(button.id), 1) //retira page_button do array de cores selecionadas
+    closerButton()
+}
+
+function onButton(button) {
+    button.style.boxShadow = "0px 0px 5px white"
+    button.style.filter = "brightness(120%)"
+    colors.push(button.id) //coloca page_button no array de cores selecionadas
+    closerButton()
+}
+
 function cleanSelections() {
     for (let pos in colors) {
-        let button = document.getElementById(`${colors[pos]}`)
+        let button = document.getElementById(colors[pos])
+
+        alert(`${button.id}`)
+        alert(`${button.id} apagado`)
+        
         offButton(button)
-        colors.splice(colors[pos], 1)
+        
+        
     }
-    closerButton()
+    status_btn_closer = true
 }
 
 function closerButton() {
@@ -34,30 +64,5 @@ function closerButton() {
         let button = document.getElementById('closer')
         button.remove()
         status_btn_closer = false
-    }
-}
-
-function offButton(button) {
-    colors.splice(colors.indexOf(button.id), 1) //retira page_button do array de cores selecionadas
-    button.style.removeProperty("filter")
-    button.style.boxShadow = "0px 0px 5px rgba(0, 0, 0, 0.384)"
-    closerButton()
-}
-
-function onButton(button) {
-    button.style.boxShadow = "0px 0px 5px white"
-    button.style.filter = "brightness(120%)"
-    colors.push(button.id) //coloca page_button no array de cores selecionadas
-    closerButton()
-}
-
-function changedButton(page_button) {
-    let button = document.getElementById(page_button.id)
-
-    if (isButtonActive(page_button)) {
-        offButton(button);
-    } else {
-        onButton(button);
-        //window.alert(`--var(${page_button.id})`)
     }
 }
