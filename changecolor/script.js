5be215c02fcdd4fb9761549ba5e5cdbd8079cef9
Jonathan Santos <jonathanjsantos@icloud.com>
2022-07-30 21:06:52 -0300

    update

--- changecolor/script.js
+++ changecolor/script.js
@@ -1,13 +1,52 @@
 var buttons = []
+var status_closer = false
 
-function changedButton(div_button) {
-    if (buttons.indexOf(`b_id:${div_button.id}`) == -1) {
+function selectedButton(div_button) {
+    if (buttons.indexOf(div_button.id) == -1) {
         onButton(div_button)
+    } else {
+        offButton(div_button)
     }
 }
 
 function onButton(div_button) {
-    let button = {b_id: div_button.id, b_status: true}
-    buttons.push(button)
+    div_button.style.boxShadow = "0px 0px 5px white"
+    div_button.style.filter = "brightness(120%)"
+
+    buttons.push(div_button.id)
+
     alert(`${buttons}`)
+
+    closerButton()
+}
+
+function offButton(div_button) {
+    div_button.style.removeProperty("filter")
+    div_button.style.boxShadow = "0px 0px 5px rgba(0, 0, 0, 0.384)"
+
+    buttons.splice(buttons.indexOf(div_button.id), 1)
+
+    alert(`Apagado`)
+    alert(`${buttons}`)
+
+    closerButton()
+}
+
+function closerButton() {
+    if (buttons.length >= 1 && status_closer == false) {
+        let button = document.createElement('div')
+        let header = document.getElementById('header')
+        button.setAttribute("id", "closer")
+        button.setAttribute("class", "selector")
+        button.setAttribute("onclick", "cleaner()")
+        button.setAttribute("background-image", "url('images/close.png')")
+        button.setAttribute("position", "center")
+        button.setAttribute("background-size", "contain")
+        header.appendChild(button)
+        status_closer = true
+    } else if (buttons.lenght == 0 && status_closer == true) {
+        status_closer = false
+        button = document.getElementById("closer")
+        button.remove()
+    }
 }
